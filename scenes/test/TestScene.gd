7cbf21537646c74ccccdf3bb571e7055c2e727c2
Jacob Ibáñez Sánchez <jacobibanez@jacobibanez.com>
2021-09-27 07:44:07 +0200

    Refactor horn slots test

--- scenes/test/TestScene.gd
+++ scenes/test/TestScene.gd
@@ -20,70 +20,65 @@ func _ready():
 	
 	test()
 
-func add_fruit(fruit: Node):
-		var remote_transform = slots.left_slots.queue()
+func add_fruit(fruit: Node, horn: String):
+		var remote_transform = slots[horn].queue()
 		if remote_transform:
 			add_child(fruit)
 			remote_transform.remote_path = fruit.get_path()
 
-func remove_fruit():
-	var dequeued = slots.left_slots.dequeue()
+func remove_fruit(horn: String):
+	var dequeued = slots[horn].dequeue()
 	if dequeued:
 		remove_child(get_node(dequeued))
 
 func test():
-	should_not_fail_when_adding_more_fruits_than_allowed()
+	should_not_fail_when_adding_and_removing_more_fruits_than_allowed("left_slots")
 	yield(self, "test_finished")
-	should_not_fail_when_removing_more_fruits_than_allowed()
+	should_add_and_remove_one_fruit("right_slots")
 	yield(self, "test_finished")
-	should_add_and_remove_one_fruit()
+	should_add_and_remove_fruits_in_order("right_slots")
 	yield(self, "test_finished")
-	should_add_and_remove_fruits_in_order()
 
-func should_not_fail_when_adding_more_fruits_than_allowed():
-	add_fruit(apple)
+func should_not_fail_when_adding_and_removing_more_fruits_than_allowed(horn: String):
+	add_fruit(apple, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	add_fruit(pear)
+	add_fruit(pear, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	add_fruit(pineapple)
+	add_fruit(pineapple, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	add_fruit(banana)
+	add_fruit(banana, horn)
+	yield(get_tree().create_timer(1.0), "timeout")
+	remove_fruit(horn)
+	yield(get_tree().create_timer(1.0), "timeout")
+	remove_fruit(horn)
+	yield(get_tree().create_timer(1.0), "timeout")
+	remove_fruit(horn)
+	yield(get_tree().create_timer(1.0), "timeout")
+	remove_fruit(horn)
 	yield(get_tree().create_timer(1.0), "timeout")
 	
 	emit_signal("test_finished")
 
-func should_not_fail_when_removing_more_fruits_than_allowed():
-	remove_fruit()
+func should_add_and_remove_one_fruit(horn: String):
+	add_fruit(apple, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
-	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
-	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
+	remove_fruit(horn)
 	yield(get_tree().create_timer(1.0), "timeout")
 	
 	emit_signal("test_finished")
 
-func should_add_and_remove_one_fruit():
-	add_fruit(apple)
+func should_add_and_remove_fruits_in_order(horn: String):
+	add_fruit(pineapple, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
+	add_fruit(pear, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	
-	emit_signal("test_finished")
-
-func should_add_and_remove_fruits_in_order():
-	add_fruit(pineapple)
+	remove_fruit(horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	add_fruit(pear)
+	add_fruit(banana, horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
+	remove_fruit(horn)
 	yield(get_tree().create_timer(1.0), "timeout")
-	add_fruit(banana)
-	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
-	yield(get_tree().create_timer(1.0), "timeout")
-	remove_fruit()
+	remove_fruit(horn)
 	yield(get_tree().create_timer(1.0), "timeout")
 	
 	emit_signal("test_finished")
