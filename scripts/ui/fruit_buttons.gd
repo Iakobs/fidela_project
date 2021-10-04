54e4c7166e5a02d0c17c542ce6c9685507f459c4
Jacob Ibáñez Sánchez <jacobibanez@jacobibanez.com>
2021-10-04 15:08:50 +0200

    Fix bug when generating fruits
    Add releases folder to .gitignore

--- scripts/ui/fruit_buttons.gd
+++ scripts/ui/fruit_buttons.gd
@@ -9,14 +9,11 @@ onready var balloon = $Balloon
 onready var progress_bar = $ProgressBarHolder/ProgressBar3D
 onready var buttons = $Buttons
 
-var pressed_fruit: int
-
 func _ready():
 	if mode == 1:
 		balloon.flip_v = true
 	
 	progress_bar.visible = false
-	progress_bar.connect("progress_completed", self, "_on_progress_completed")
 	
 	for child in buttons.get_children():
 		child.horn = horn
@@ -30,18 +27,17 @@ func subscriptions() -> Array:
 	]
 
 func _on_fruit_generation_start(payload: Dictionary):
-	pressed_fruit = payload.fruit
+	var pressed_fruit = payload.fruit
 	var pressed_horn = payload.horn
 	if pressed_horn == horn:
 		var generation_time = Global.get_generation_time(pressed_fruit)
 		disable_during_generation(generation_time)
-
-func _on_progress_completed():
-	enable()
-	EventBus.publish(
-		EventNamespaces.FruitEvents.GENERATION_END_EVENT,
-		{ "fruit": pressed_fruit, "horn": horn }
-	)
+		yield(progress_bar, "progress_completed")
+		enable()
+		EventBus.publish(
+			EventNamespaces.FruitEvents.GENERATION_END_EVENT,
+			{ "fruit": pressed_fruit, "horn": horn }
+		)
 
 func disable_during_generation(dutation: float):
 	progress_bar.visible = true
